53b33d6e036085160e51fd645249492a74975561
Classified56 <Davis.Ranney@MU-C1-01.mullen.pvt.k12.co.us>
2015-09-24 08:44:00 -0600

    Deck solved. On to the game itself

--- Blackjack/src/Blackjack.java
+++ Blackjack/src/Blackjack.java
@@ -1,17 +1,10 @@
-import java.util.ArrayList;
-import java.util.Collections;
 
 public class Blackjack
 	{
 		public static void main(String[] args)
 			{
 				Deck shoe = new Deck();
-				for(int i = 0; i < 52; i++)
-					{
-						System.out.println(shoe.getCard(i).getSuit());
-						System.out.println(shoe.getCard(i).getFace());
-						System.out.println(shoe.getCard(i).getValue());
-					}
+				System.out.println(shoe);
 			}
 
 	}
--- Blackjack/src/Deck.java
+++ Blackjack/src/Deck.java
@@ -9,47 +9,77 @@ public class Deck
 				int value = 0;
 				String face = "";
 				String suit = "";
-				for(int i = 65; i < 69; i++)
+				for(int i = 0; i < 4; i++)
 					{
-						int suitSet = 68 - i;
 						for(int j = 1; j <= 13; j++)
 							{
 								switch(j)
-								{
-									case 1:
-										value = 11;
-										face = "A";
-										break;
-									
-									case 11: 
-										face = "J";
-										value = 10;
-										break;
-									case 12: 
-										face = "Q";
-										value = 10;
-										break;
-									case 13: 
-										face = "K";
-										value = 10;
-										break;
-									default:
-										face = j;
-										value = j;
-										break;
-								}
-								switch(suitSet)
 									{
 										case 1:
-											suit = "Hearts";
+											value = 11;
+											face = "A";
 											break;
 										case 2:
-											suit = "Spades";
+											face = "2";
+											value = j;
 											break;
 										case 3:
-											suit = "Diamonds";
+											face = "3";
+											value = j;
 											break;
 										case 4:
+											face = "4";
+											value = j;
+											break;
+										case 5:
+											face = "5";
+											value = j;
+											break;
+										case 6:
+											face = "6";
+											value = j;
+											break;
+										case 7:
+											face = "7";
+											value = j;
+											break;
+										case 8:
+											face = "8";
+											value = j;
+											break;
+										case 9:
+											face = "9";
+											value = j;
+											break;
+										case 10:
+											face = "10";
+											value = j;
+											break;
+										case 11: 
+											face = "J";
+											value = 10;
+											break;
+										case 12: 
+											face = "Q";
+											value = 10;
+											break;
+										case 13: 
+											face = "K";
+											value = 10;
+											break;
+									}
+								switch(i)
+									{
+										case 0:
+											suit = "Hearts";
+											break;
+										case 1:
+											suit = "Spades";
+											break;
+										case 2:
+											suit = "Diamonds";
+											break;
+										case 3:
 											suit = "Clubs";
 											break;
 									}
@@ -57,9 +87,13 @@ public class Deck
 							}
 					}
 			}
-		public Card getCard(int spot)
+		public Card drawCard(int spot)
 			{
 				return deck.get(spot);
+			}
+		public void removeCard(int spot)
+			{
+				deck.remove(spot);
 			}
 		public void setCard(int spot, String suit, int value, String face)
 			{
@@ -67,10 +101,14 @@ public class Deck
 				deck.get(spot).setValue(value);
 				deck.get(spot).setFace(face);
 			}
-		public ArrayList shuffleDeck(ArrayList d)
+		public ArrayList<Card> shuffleDeck()
 			{
-				Collections.shuffle(d);
-				return d;
+				Collections.shuffle(deck);
+				return deck;
+			}
+		public void addCard(Card one)
+			{
+				deck.add(one);
 			}
 		public int size()
 			{
